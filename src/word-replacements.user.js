74183e44d680712996d72c6db6b3526a10b9096d
PurpleMagick <PurpleMagick@users.noreply.github.com>
2020-06-20 14:12:33 +0300

    Refactored the code
    Changed from a straight script to function based for more flexibility

--- src/word-replacements.user.js
+++ src/word-replacements.user.js
@@ -3,7 +3,7 @@
 // @namespace		https://github.com/PurpleMagick/
 // @description		Replaces the word "folk" with other "people"
 // @author			VLAZ
-// @version			0.0.1
+// @version			0.0.2
 //
 // @include			/^https:\/\/(?:meta\.)?stackoverflow\.com/
 // @include			/^https:\/\/(?:meta\.)?serverfault\.com/
@@ -22,7 +22,7 @@
 // ==/UserScript==
 
 const rules = new Map([
-//   match                         replacement
+//       match                 replacement
 	[/\b(f)olks\b/ig,          (_, first) => {
 		let p = "p";
 		if (first.toUpperCase() === first)
@@ -33,21 +33,27 @@ const rules = new Map([
 ]);
 
 const runReplacements = searchAndReplace(rules);
-const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
 
-let node;
-// eslint-disable-next-line no-cond-assign
-while(node = walker.nextNode()) {
-	runReplacements(node);
-}
-
-function searchAndReplace(what) {
-	return function(textNode) {
-
+function searchAndReplace(replacementConfig) {
+	return function replaceAllWords(textNode) {
 		let result = textNode.nodeValue;
-		for(const [regex, replacement] of what) {
+
+		for(const [regex, replacement] of replacementConfig) {
 			result = result.replace(regex, replacement);
 		}
+
 		textNode.textContent = result;
 	};
 }
+
+function execute(startNode) {
+	const walker = document.createTreeWalker(startNode, NodeFilter.SHOW_TEXT, null, false);
+
+	let node;
+	// eslint-disable-next-line no-cond-assign
+	while(node = walker.nextNode()) {
+		runReplacements(node);
+	}
+}
+
+execute(document.body);
